7b95a60f86d5971331dfd8f61078d85815982f68
Musab Khan <161490790+musab05@users.noreply.github.com>
2026-02-19 11:12:50 +0530

    Updated 1.3

---
+++ 1.3/myproject/MyProgram.java
@@ -0,0 +1,25 @@
+// 1. Documentation Section (Optional)
+/**
+ * This program prints a message to the console.
+ */
+
+// 2. Package Statement (Optional)
+package myproject;
+
+// 3. Import Statements (Optional)
+import java.util.Scanner;
+
+// 4. Class Definition (Mandatory)
+public class MyProgram {
+
+    // 5. Main Method (Entry Point)
+    public static void main(String[] args) {
+        
+        // 6. Statements/Logic
+        System.out.println("Hello, Java!");
+    }
+}
+
+
+// Run - javac myproject\MyProgram.java
+// Then followed by - java myproject.MyProgram
